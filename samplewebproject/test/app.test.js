ad75837fffe47be20e21c761bd044065692da37a
stackblock <lambdaTL.anthony@gmail.com>
2019-12-27 17:08:15 -0500

    really done now

--- samplewebproject/test/app.test.js
+++ samplewebproject/test/app.test.js
@@ -22,3 +22,17 @@ it("shows a success message with a valid email", async () => {
 
   assert.strictEqual(h1.innerHTML, "looks good");
 });
+
+it("shows a fail message with a valid email", async () => {
+    const dom = await render("index.html");
+  
+    const input = dom.window.document.querySelector("input");
+    input.value = "asdfadfjkh";
+    dom.window.document
+      .querySelector("form")
+      .dispatchEvent(new dom.window.Event("submit"));
+  
+    const h1 = dom.window.document.querySelector("h1");
+  
+    assert.strictEqual(h1.innerHTML, "Invalid email");
+  });
